f1551fb90d408885b8d9f5245ee2a8c665d627a1
tvasutin <vasutin@iac.spb.ru>
2019-10-10 17:23:26 +0300

    test

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+import org.apache.log4j.Logger;
+
+public class Hello {
+    private static final Logger LOG = Logger.getLogger(Hello.class);
+    public static void main(String[] args) {
+        LOG.info("Application started");
+        System.out.println("Hello World");
+        LOG.info("Application finished");
+    }
+}
